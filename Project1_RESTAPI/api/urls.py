5b86013624842841851a76729009d9e51e65e987
Harish141296 <harishparamathmananda@gmail.com>
2024-09-29 06:48:16 +0530

    BlogPostList_APIView

--- Project1_RESTAPI/api/urls.py
+++ Project1_RESTAPI/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('blogposts/',views.BlogPostListCreate.as_view(), name='BlogPostListCreate'),
     path('blogposts/<int:pk>/', views.BlogPostRetrieveUpdateDestroy.as_view(), name='update'),
+    path('blogposts/new/', views.BlogPostList.as_view(), name='BlogPostList'),
 
 ]
